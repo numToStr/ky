bfc1b2c4515ba5bd0365dd84d5038623e3501be5
numtostr <vikasraj1911@gmail.com>
2021-06-08 19:22:14 +0530

    cmd(gen): added --qr-code flag to print password in a qr-code

--- src/subcommand/gen.rs
+++ src/subcommand/gen.rs
@@ -1,6 +1,6 @@
 use crate::{
     cli::{Config, PasswordParams},
-    lib::{KyError, Password},
+    lib::{KyError, Password, Qr},
     subcommand::Command,
 };
 use clap::Clap;
@@ -9,15 +9,30 @@ use clap::Clap;
 pub struct Generate {
     #[clap(flatten)]
     pwd_opt: PasswordParams,
+
+    /// Show password in a form of qr code
+    #[clap(short, long)]
+    qr_code: bool,
+
+    /// Don't print the details, can be used with qr code
+    #[clap(short, long)]
+    mute: bool,
 }
 
 impl Command for Generate {
     fn exec(&self, _: Config) -> Result<(), KyError> {
         let password = Password::generate(&self.pwd_opt);
 
-        // Printing first part to stderr so that password can be easily read from stdout
-        eprint!("Password: ");
-        println!("{}", password);
+        if self.qr_code {
+            let code = Qr::new(&password.to_string()).render();
+            print!("{}", code);
+        }
+
+        if !self.mute {
+            // Printing first part to stderr so that password can be easily read from stdout
+            eprint!("Password: ");
+            println!("{}", password);
+        }
 
         Ok(())
     }
