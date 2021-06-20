02f610c606db180bb327b3c7caa9e3cfe45f36de
numtostr <vikasraj1911@gmail.com>
2021-06-20 19:03:19 +0530

    added KyEnv and KyDb struct

--- src/lib/database.rs
+++ src/lib/database.rs
@@ -15,24 +15,121 @@ macro_rules! check_db {
     };
 }
 
-type DatabaseType = Mdbx<Str, Str>;
+type KyDbType = Mdbx<Str, Str>;
 
 /// KyTable is a collection of all the table names
-enum KyTable {
+pub enum KyTable {
+    Master,
     Password,
 }
 
 impl Display for KyTable {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
+            Self::Master => f.write_str("master"),
             Self::Password => f.write_str("password"),
         }
     }
 }
 
+/// KyDb holds a individual database and its methods
+pub struct KyDb {
+    db: KyDbType,
+}
+
+impl KyDb {
+    pub fn new(env: &Env, name: &KyTable) -> Result<Self, KyError> {
+        let db: KyDbType = env
+            .create_database(Some(&name.to_string()))
+            .map_err(|_| KyError::Connection)?;
+
+        Ok(Self { db })
+    }
+
+    pub fn set(&self, wtxn: &mut RwTxn, key: &str, val: &str) -> Result<(), KyError> {
+        let res = self
+            .db
+            .put(wtxn, key, val)
+            .map_err(|_| KyError::Set(key.to_string()))?;
+
+        Ok(res)
+    }
+
+    pub fn get(&self, rtxn: &RoTxn, key: &str) -> Result<String, KyError> {
+        let bytes = self
+            .db
+            .get(&rtxn, key)
+            .map_err(|_| KyError::Get(key.to_string()))?;
+
+        match bytes {
+            Some(x) => Ok(x.to_string()),
+            _ => Err(KyError::NotFound(key.to_string())),
+        }
+    }
+
+    pub fn delete(&self, wtxn: &mut RwTxn, key: &str) -> Result<bool, KyError> {
+        let is_deleted = self
+            .db
+            .delete(wtxn, key)
+            .map_err(|_| KyError::Delete(key.to_string()))?;
+
+        Ok(is_deleted)
+    }
+
+    pub fn ls(&self, rtxn: &RoTxn) -> Result<Vec<(String, String)>, KyError> {
+        let mut keys: Vec<(String, String)> = Vec::new();
+
+        for kv in self.db.iter(rtxn)? {
+            let (k, v) = kv?;
+
+            if k != MASTER {
+                keys.push((k.to_string(), v.to_string()));
+            }
+        }
+
+        Ok(keys)
+    }
+}
+
+/// KyEnv hold the lmdb environment and database connection
+pub struct KyEnv {
+    env: Env,
+}
+
+impl KyEnv {
+    pub fn connect(path: &Path) -> Result<Self, KyError> {
+        let env = EnvOpenOptions::new()
+            .max_dbs(5)
+            .open(path)
+            .map_err(|_| KyError::Connection)?;
+
+        Ok(Self { env })
+    }
+
+    pub fn get_table(&self, name: KyTable) -> Result<KyDb, KyError> {
+        KyDb::new(&self.env, &name)
+    }
+
+    pub fn write_txn(&self) -> Result<RwTxn, KyError> {
+        let wtxn = self.env.write_txn()?;
+
+        Ok(wtxn)
+    }
+
+    pub fn read_txn(&self) -> Result<RoTxn, KyError> {
+        let rtxn = self.env.read_txn()?;
+
+        Ok(rtxn)
+    }
+
+    pub fn close(self) {
+        self.env.prepare_for_closing().wait();
+    }
+}
+
 pub struct Database {
     env: Env,
-    conn: DatabaseType,
+    conn: KyDbType,
 }
 
 impl Database {
@@ -45,7 +142,7 @@ impl Database {
         let table = KyTable::Password.to_string();
 
         // we will open the default unamed database
-        let conn: DatabaseType = env
+        let conn: KyDbType = env
             .create_database(Some(&table))
             .map_err(|_| KyError::Connection)?;
 
